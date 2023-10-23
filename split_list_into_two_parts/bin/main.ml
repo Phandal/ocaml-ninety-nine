82d591942d107e16281cc056daa9b65ed6d0478d
Phandal <bfeld.111@gmail.com>
2023-10-22 20:05:06 -0500

    initial commit of splitting a list

---
+++ split_list_into_two_parts/bin/main.ml
@@ -0,0 +1,4 @@
+(* Split a list into two parts; the length of the first part is given.
+   If the length of the first part is longer than the entire list, then the first part is the list and the second part is empty *)
+
+let () = Printf.printf "%s" "hello"
